d052ce8e9f5928e052a07edc184310e29ba9c031
博士 <clarenelson@163.com>
2021-08-14 23:03:15 +0800

    修改lua代码，有了基础的加载lua脚本的功能

--- GameFramework/Assets/Scripts/Lua/LuaManager.cs
+++ GameFramework/Assets/Scripts/Lua/LuaManager.cs
@@ -8,8 +8,10 @@ using XLua;
 
 public class LuaManager : UnitySingleton<LuaManager>
 {
-    private static string luaScriptsPath = "LocalResources/0.Lua-ab/Utility";
+    private static string luaScriptsPath = "/LocalResources/0.Lua-ab/";
     private static LuaEnv _luaEnv = null;
+    private Dictionary<string, string> luaFileDic_Editor;
+    private Dictionary<string, string> luaFileDic;
     
     public static LuaEnv luaEnv
     {
@@ -27,20 +29,66 @@ public class LuaManager : UnitySingleton<LuaManager>
     public override void Awake()
     {
         base.Awake();
+#if UNITY_EDITOR
+        luaFileDic_Editor = new Dictionary<string, string>();
+        luaFileDic_Editor = LoadAlllLuaFile(Application.dataPath+luaScriptsPath, luaFileDic_Editor);
+#else
+        luaFileDic = new Dictionary<string, string>();
+        luaFileDic = LoadAlllLuaFile(Application.dataPath+luaScriptsPath, luaFileDic);
+#endif
         luaEnv.AddLoader(CustomLoader);
     }
 
-    public static byte[] CustomLoader(ref string filePath)
+    private Dictionary<string, string> LoadAlllLuaFile(string luaPath, Dictionary<string, string> dictionary)
     {
-        string scriptPath = string.Empty;
-        filePath = filePath.Replace(".", "/") + ".lua.txt";
-
+        DirectoryInfo directoryInfo = new DirectoryInfo(luaPath);
+        var files = directoryInfo.GetFiles("*.lua.txt");
+        var dii = directoryInfo.GetDirectories();
+        for (int i = 0; i < files.Length; i++)
+        {
 #if UNITY_EDITOR
-        scriptPath = Path.Combine(Application.dataPath, luaScriptsPath);
-        scriptPath = Path.Combine(scriptPath, filePath);
+            if (luaFileDic_Editor.ContainsKey(files[i].Name))
+            {
+                Debug.LogError("存在重名的lua文件："+files[i].Name);
+            }
+            else
+            {
+                luaFileDic_Editor.Add(files[i].Name, files[i].FullName);   
+            }
+#else
+            if (luaFileDic.ContainsKey(files[i].Name))
+            {
+                Debug.LogError("存在重名的lua文件："+files[i].Name);
+            }
+            else
+            {
+                luaFileDic.Add(files[i].Name, files[i].FullName);   
+            }
+#endif
+        }
 
-        byte[] data = System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(scriptPath));
-        return data;
+        foreach (DirectoryInfo d in dii)
+        {
+            LoadAlllLuaFile(d.FullName, dictionary);
+        }
+
+        return dictionary;
+    }
+
+    public byte[] CustomLoader(ref string filePath)
+    {
+        filePath = filePath.Replace(".", "/") + ".lua.txt";
+#if UNITY_EDITOR
+        if (luaFileDic_Editor.ContainsKey(filePath))
+        {
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(luaFileDic_Editor[filePath]));
+            return data;
+        }
+        else
+        {
+            Debug.LogError("找不到要加载的lua文件："+filePath);
+            return null;
+        }
 #elif !UNITY_EDITOR && UNITY_IOS
 
 #elif !UNITY_EDITOR && UNITY_ANDROID
